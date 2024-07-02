4fad146b1390b07057c95153eead5a110c793e7f
Mykhailo Kadenko <mishakadenko@mail.ru>
2024-07-03 05:53:18 +0900

    Generated Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :trees
+  get 'home/index'
+  get "up" => "rails/health#show", as: :rails_health_check
+  root "home#index"
 end
