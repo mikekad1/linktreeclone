d99aff0ef8043eafbb818f8ac982ebf0d52d0548
Mykhailo Kadenko <mishakadenko@mail.ru>
2024-07-05 15:01:59 +0900

    Added Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get 'home/index'
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
+  devise_scope :user do
+    #get '/users', to: 'devise/registrations#new'
+    #get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
